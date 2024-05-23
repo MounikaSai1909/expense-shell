3a5b5172ceee762f299340e3b297d88112acf438
putti swamy <puttiswamy123@gmail.com>
2024-05-23 20:23:31 +0530

    sample file

--- mysql.sh
+++ mysql.sh
@@ -11,29 +11,35 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-     echo -e "$2  $R  FAILURE $N "
+       echo -e "$2...$R  FAILURE $N"
+       exit 1
     else 
-     echo -e "$2  $G  SUCCESS $N "
+       echo -e "$2... $G  SUCCESS $N"
     fi
 }
 
 if [ $USERID -ne 0 ]
 then 
-    echo " Please run with super user "
+    echo "Please run with super user"
+    exit 1
 else 
-    echo " You are a super user"
+    echo "You are a super user"
 fi
 #
 #
 #
-dnf install mysql-server -y &>> $LOGFILE
+dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "installing mysql server"
 
-systemctl enable mysqld &>> $LOGFILE
+systemctl enable mysqld &>>$LOGFILE
 VALIDATE $? "enabling mysqld service"
 
-systemctl start mysqld &>> $LOGFILE
+systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "staring mysql service"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>> $LOGFILE
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 VALIDATE $? "setting up the root password"
+
+
+
+
